9c4f029b320b7605829664b5b610c3dc2d2bde79
0xV3TO <131310147+0xV3TO@users.noreply.github.com>
2023-04-22 00:24:41 +0000

    Add files via upload

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game();
+            
+        }
+        public static void Game()
+        {
+            int playerScore = 0;
+            int computerScore = 0;
+
+            while (true)
+            {
+                if (playerScore == 3)
+                {
+                    Utils.YouWin();
+                    Utils.PlayAgain();
+                }
+                else if (computerScore == 3)
+                {
+                    Utils.YouLose();
+                    Utils.PlayAgain();
+                }
+                Console.WriteLine(@"
+__________ _________   ____  __.    __________ __________ __________       __________________ __________   _________ 
+\______   \\_   ___ \ |    |/ _|    \______   \\______   \\______   \     /   _____/\_   ___ \\______   \ /   _____/ 
+ |       _//    \  \/ |      <       |     ___/ |     ___/ |       _/     \_____  \ /    \  \/ |       _/ \_____  \  
+ |    |   \\     \____|    |  \      |    |     |    |     |    |   \     /        \\     \____|    |   \ /        \ 
+ |____|_  / \______  /|____|__ \     |____|     |____|     |____|_  /    /_______  / \______  /|____|_  //_______  / 
+        \/         \/         \/                                  \/             \/         \/        \/         \/  
+             -----------------------------------------------------------------------------------------------------
+                            
+");
+                string[] choices = { "rock", "paper", "scissors" };
+                // Getting and checking user choice.
+                string playerChoice = Utils.getPlayerInput();
+                bool isChoiceValid = false;
+                foreach (string choice in choices)
+                {
+                    if (choice == playerChoice)
+                    {
+                        isChoiceValid = true;
+                    }
+                }
+                if (!isChoiceValid)
+                {
+                    Utils.Exit("Invalid Choice");
+                }
+                // Getting computer choice.
+                Random random = new Random();
+                int randInt = random.Next(0, 3);
+                string computerChoice = choices[randInt];
+
+                // Comparing choices.
+                if (playerChoice == computerChoice) {
+                    Utils.ShowChoices(playerChoice, computerChoice, playerScore, computerScore);
+                    continue;
+                } else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper"))
+                {
+                    playerScore += 1;
+                    Utils.ShowChoices(playerChoice, computerChoice, playerScore, computerScore);
+                    continue;
+                } else
+                {
+                    computerScore += 1;
+                    Utils.ShowChoices(playerChoice, computerChoice, playerScore, computerScore);
+                    continue;
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ RockPaperScissors/Utils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class Utils
+    {
+        public static string getPlayerInput()
+        {
+            // prompting to the user to get a choice.
+            Console.Write("enter your choice (rock,paper or scissors): ");
+            try
+            {
+                string choice = (Console.ReadLine()).ToLower();
+                return choice;
+            } catch (Exception e)
+            {
+                Exit(e.Message);
+                return "";
+            }
+            
+        }
+        public static void Exit(string cause)
+        {
+            Console.Clear();
+            Console.WriteLine("Exiting... {0}", cause);
+            Environment.Exit(0);
+        }
+        public static void ShowChoices(string player, string computer, int playerScore, int computerScore)
+        {
+            Console.WriteLine("\nYou chosen: {0}", player);
+            Console.WriteLine("Computer chosen: {0}", computer);
+
+            Console.Write("you {0} - {1} computer.", playerScore, computerScore);
+        }
+        public static void YouWin()
+        {
+            Console.WriteLine("You Win!");
+            PlayAgain();
+        }
+        public static void YouLose()
+        {
+            Console.WriteLine("You Lose!");
+            PlayAgain();
+        }
+        public static void PlayAgain()
+        {
+            Console.Write("Do you want to play again? (yes/no): ");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes")
+            {
+                Console.Clear();
+                Program.Game();
+            } else
+            {
+                Exit("Goodbye!");
+            }
+        }
+    }
+}
